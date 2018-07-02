57aaadd51a6f2ca911bc965476f19e9e42f17504
Megan Evans <megan.evans.ducky@gmail.com>
2018-07-01 19:47:17 -0600

    finished the momnet js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,10 +12,11 @@ $(document).ready(function () {
     firebase.initializeApp(config);
 
     var database = firebase.database();
+
     var name = "Furry";
     var dest = "KittyLand";
-    var arrive = "Purr in the Morning";
-    var freq = "Right Meow";
+    var firstArrive = "00:00";
+    var freq = 0;
 
     var ref = database.ref('trains');
 
@@ -31,41 +32,66 @@ $(document).ready(function () {
 
     })
 
-    database.ref().on("value", function(snapshot){
-        cycleThrough();
-    })
+    // dataRef = database.ref("trains/");
 
-    function cycleThrough() {
-        var cycle = database.ref("trains").orderByKey();
-        cycle.once("value")
-            .then(function (snapshot) {
-                snapshot.forEach(function (childSnapshot) {
-                    var key = childSnapshot.key;
-                    var childData = childSnapshot.val();
+    var cycle = database.ref("trains").orderByChild("name");
+    cycle.on("child_added", function (childSnapshot) {
 
-                    dest = childData.dest;
-                    arrive = childData.arrive;
-                    freq = childData.freq;
-                    name = childData.name;
 
-                    var newTr = $("<tr>");
-                    var newTh = $("<th class='left' scope='row'>");
-                    newTh.text(name);
+        // var key = childSnapshot.key;
+        // console.log("current " + moment(currentTime).format("hh:mm"))
 
-                    var destTd = $("<td>").text(dest);
-                    var arriveTd = $("<td>").text(arrive);
-                    var freqTd = $("<td>").text(freq);
-                    var minAway = $("<td>").text("30 minutes");
-                    //minAway will equal the next arrival time minus the current time, then displayed on screen
+        dest = childSnapshot.val().dest;
+        firstArrive = childSnapshot.val().arrive;
+        freq = parseInt(childSnapshot.val().freq);
+        name = childSnapshot.val().name;
+        // console.log(firstArrive)
 
-                    newTr.append(newTh)
-                    newTr.append(destTd)
-                    newTr.append(arriveTd)
-                    newTr.append(freqTd);
-                    newTr.append(minAway);
+        var convFormat = "HH:mm";
+        var currentTime = moment();
+    
+        // freq = 20;
+        // firstArrive = "14:00";
+    
+        var firstC = moment(firstArrive, "HH:mm").subtract(1, "years");
+        console.log("current " + moment(currentTime).format("hh:mm"))
+    
+    
+        console.log("converted" + firstC);
+        var diffTime = moment().diff(moment(firstC), "minutes");
+        console.log("difference: " + diffTime)
+    
+        var remainder = diffTime % parseInt(freq);
+    
+        var minutesNextTrain = parseInt(freq) - remainder;
+        console.log("minutes till next: " + minutesNextTrain);
+    
+        var nextTrain = moment().add(minutesNextTrain, "minutes");
+        var nextFormated = moment(nextTrain).format("hh:mm")
 
-                    $(".table-body").append(newTr);
-                });
-            });
-    }
+        // //calculate minutes away
+
+        var newTr = $("<tr>");
+        var newTh = $("<th class='left' scope='row'>");
+        newTh.text(name);
+
+        var destTd = $("<td>").text(dest);
+        var arriveTd = $("<td>").text(nextFormated);
+        var freqTd = $("<td>").text(freq);
+        var minAway = $("<td>").text(minutesNextTrain);
+        //minAway will equal the next arrival time minus the current time, then displayed on screen
+
+        newTr.append(newTh)
+        newTr.append(destTd)
+        newTr.append(arriveTd)
+        newTr.append(freqTd);
+        newTr.append(minAway);
+
+        $(".table-body").append(newTr);
+    });
+
+
+
+    //calculate minutes away
+
 });
